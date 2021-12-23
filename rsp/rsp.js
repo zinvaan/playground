9315afef60dd4e333410ab9e54ef5d4689d5573f
zinvaan <wonjin89jung@gmail.com>
2021-12-23 14:57:15 +0900

    가위바위보(setTimeout을 이용하여 0.05초마다 이미지 반복)

--- rsp/rsp.js
+++ rsp/rsp.js
@@ -6,10 +6,28 @@ const $paper = document.querySelector('#paper');
 
 const IMG_URL = './rsp.png'; // 가위바위보 이미지
 $computer.style.background = `url(${IMG_URL}) 0 0`; // 가위
-$computer.style.background = `url(${IMG_URL}) -220px 0`; // 바위
-$computer.style.background = `url(${IMG_URL}) -440px 0`; // 보
 $computer.style.backgroundSize = 'auto 200px'; // backgroundSize CSS 속성은 background 속성과 항상 같이 나와야함.
 
-const scissorsX = '-0'; // 가위
-const rockX = '-220px'; // 바위
-const paperX = '-440px'; // 보
+//객체로 묶기
+const rspX={
+  scissors: '-0', // 가위
+  rock: '-220px', // 바위
+  paper: '-440px', // 보
+}
+
+let computerChoice = 'scissors'; // 컴퓨터가 처음에 가위를 내도록 설정
+const changeComputerHand = () => {
+  if(computerChoice === 'rock'){
+    computerChoice = 'scissors';
+  }
+  else if(computerChoice === 'scissors'){
+    computerChoice = 'paper';
+  }
+  else if(computerChoice === 'paper'){
+    computerChoice = 'rock';
+  }
+  $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
+  $computer.style.backgroundSize = 'auto 200px';
+  setTimeout(changeComputerHand, 50); // 함수가 끝나기 직전에 다시 타이머 작동시킴
+}
+setTimeout(changeComputerHand, 50); // 0.05초 마다 함수 실행
