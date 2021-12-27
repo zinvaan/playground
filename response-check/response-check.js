e4a3cc522f048295d132479c74f02946e5204188
zinvaan <wonjin89jung@gmail.com>
2021-12-27 22:35:03 +0900

    반응속도테스트(타이머 추가:무작위로 시간지정-대기화면2)

--- response-check/response-check.js
+++ response-check/response-check.js
@@ -2,17 +2,20 @@ const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
 
 $screen.addEventListener('click', function(){
-    if($screen.classList.contains('waiting')){
-        //대기화면
+    if($screen.classList.contains('waiting')){//대기화면
         $screen.classList.remove('waiting');
         $screen.classList.add('ready');
         $screen.textContent = '초록색이 되면 클릭하세요';
+        setTimeout(function(){
+            $screen.classList.remove('ready');
+            $screen.classList.add('now');
+            $screen.textContent = '클릭하세요!';
+        }, Math.floor(Math.random()*1000)+2000); // 2000~3000초 사이 수
+    }
+    else if($screen.classList.contains('ready')){//준비화면
+
+    }
+    else if($screen.classList.contains('now')){//클릭화면
         
-    }
-    else if($screen.classList.contains('ready')){
-        //준비화면
-    }
-    else if($screen.classList.contains('now')){
-        //클릭화면
     }
 })
