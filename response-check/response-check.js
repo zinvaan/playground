1ace5e8ec3af935f7d9210f785195289b2f3449b
zinvaan <wonjin89jung@gmail.com>
2021-12-27 22:18:16 +0900

    반응속도테스트(이벤트리스너 추가하여 화면별로 다르게 작동1)

--- response-check/response-check.js
+++ response-check/response-check.js
@@ -1,2 +1,14 @@
 const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
+
+$screen.addEventListener('click', function(){
+    if($screen.classList.contains('waiting')){
+        //대기화면
+    }
+    else if($screen.classList.contains('ready')){
+        //준비화면
+    }
+    else if($screen.classList.contains('now')){
+        //클릭화면
+    }
+})
