f99b5b913cdaa5fd79b712e6c16c93d2dc6fbb08
zinvaan <wonjin89jung@gmail.com>
2021-12-28 10:53:53 +0900

    반응속도테스트(Date를 사용해 반응속도 체크 추가)

--- response-check/response-check.js
+++ response-check/response-check.js
@@ -1,12 +1,16 @@
 const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
 
+let startTime;
+let endTime;
+
 $screen.addEventListener('click', function(){
     if($screen.classList.contains('waiting')){//대기화면
         $screen.classList.remove('waiting');
         $screen.classList.add('ready');
         $screen.textContent = '초록색이 되면 클릭하세요';
         setTimeout(function(){
+            startTime = new Date();
             $screen.classList.remove('ready');
             $screen.classList.add('now');
             $screen.textContent = '클릭하세요!';
@@ -16,6 +20,10 @@ $screen.addEventListener('click', function(){
 
     }
     else if($screen.classList.contains('now')){//클릭화면
-        
+        endTime = new Date();
+        $result.textContent = `${endTime - startTime}ms`;
+        $screen.classList.remove('now');
+        $screen.classList.add('waiting');
+        $screen.textContent = '클릭해서 시작하세요.';
     }
 })
