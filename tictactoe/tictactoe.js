cdf2d6bfc3a8fdce33c721c66eb910d26d2372c4
zinvaan <wonjin89jung@gmail.com>
2021-12-31 23:25:04 +0900

    틱택토게임(3x3빈칸만들기)

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,3 +1,18 @@
 const {body} = document;
 const $table = document.createElement('table');
 const $result = document.createElement('div'); // 결과창
+const rows =[];
+
+for(let i=1;i<=3;i++){
+    const $tr = document.createElement('tr');
+    const cells =[];
+    for(let j=1;j<=3;j++){
+        const $td = document.createElement('td');
+        cells.push($td);
+        $tr.appendChild($td);
+    }
+    rows.push(cells);
+    $table.appendChild($tr);
+}
+body.appendChild($table);
+body.appendChild($result);
