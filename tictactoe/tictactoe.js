770a655ef228474e2b89d19dd2c90143b2236ccb
zinvaan <wonjin89jung@gmail.com>
2021-12-31 23:35:56 +0900

    틱택토게임(클릭하면 O 넣기)

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -2,12 +2,23 @@ const {body} = document;
 const $table = document.createElement('table');
 const $result = document.createElement('div'); // 결과창
 const rows =[];
+let turn = 'O';
 
+const callback = (event) =>{
+    if(event.target.textContent !==''){ // 칸이 이미 채워져 있으면,
+        console.log('빈칸이 아닙니다.');
+    }
+    else{ // 빈칸이면,
+        console.log('빈칸입니다.');
+        event.target.textContent = turn;
+    }
+}
 for(let i=1;i<=3;i++){
-    const $tr = document.createElement('tr');
+    const $tr = document.createElement('tr'); // tr:표의 가로줄
     const cells =[];
     for(let j=1;j<=3;j++){
-        const $td = document.createElement('td');
+        const $td = document.createElement('td'); // td:표의 세로줄
+        $td.addEventListener('click', callback);
         cells.push($td);
         $tr.appendChild($td);
     }
