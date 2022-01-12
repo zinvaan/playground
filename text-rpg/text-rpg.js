6cf7cf5b9c446494cecf6185c92daabb978b6fd6
zinvaan <wonjin89jung@gmail.com>
2022-01-12 13:24:14 +0900

    텍스트RPG(전투결과 구현)

--- text-rpg/text-rpg.js
+++ text-rpg/text-rpg.js
@@ -67,7 +67,7 @@ class Game{
     
         }
         else if(input === '3'){ // 종료
-    
+            this.quit();
         }
     }
     onBattleMenuInput = (event) =>{
@@ -77,9 +77,22 @@ class Game{
             const {hero, monster} = this;
             hero.attack(monster);
             monster.attack(hero);
-            this.showMessage(`${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`);
+            if(hero.hp <= 0){ // 주인공 사망
+                this.showMessage(`${hero.lev} 레벨에서 전사, 새 주인공을 생성하세요.`);
+                this.quit(); // game over
+            }
+            else if(monster.hp < 0){ // 몬스터 사망
+                this.showMessage(`몬스터를 잡아 ${monster.xp} 경험치를 얻었다.`);
+                hero.getXp(monster.xp);
+                this.monster = null;
+                this.changeScreen('game');
+            }
+            else{
+                this.showMessage(`${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`);
+            }
             this.updateHeroStat();
             this.updateMonsterStat();
+
         }
         else if(input === '2'){ // 회복
     
@@ -87,6 +100,16 @@ class Game{
         else if(input === '3'){ // 도망
     
         }
+    }
+    quit(){ // 모든 정보 초기화
+        this.hero = null;
+        this.monster = null;
+        this.updateHeroStat();
+        this.updateMonsterStat();
+        $gameMenu.removeEventListener('submit', this.onGameMenuInput);
+        $battleMenu.removeEventListener('submit', this.onBattleMenuInput);
+        this.changeScreen('start');
+        game = null;
     }
     updateHeroStat(){
         const {hero} = this;
@@ -137,6 +160,17 @@ class Hero{
     heal(monster){
         this.hp += 20;
         this.hp -= monster.att;
+    }
+    getXp(xp){
+        this.xp += xp;
+        if(this.xp >= this.lev * 15){ // 경험치를 다 채우면,
+            this.xp -= this.lev * 15;
+            this.lev += 1;
+            this.maxHp += 5;
+            this.att += 5;
+            this.hp = this.maxHp;
+            this.game.showMessage(`레벨업! 레벨 ${this.lev}`);
+        }
     }
 }
 
