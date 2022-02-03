eb8742c8049cbb691247e27dc46d8efff8921efd
zinvaan <wonjin89jung@gmail.com>
2022-02-03 23:06:35 +0900

    두더지잡기게임(두더지 반복 적으로 나타내기)

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -12,11 +12,13 @@ $start.addEventListener('click', () => {
     if(started) return; // 이미 시작하였으면 무시
     started = true;
     console.log('시작');
+    const tickId = setInterval(tick, 1000);
     tick();
 });
 
 function tick(){
     holes.forEach((hole, index) => {
+        if(hole) return; // 무언가 일어나고 있으면 return
         const $gopher = $$cells[index].querySelector('.gopher');
         holes[index] = setTimeout(() => { // 1초 뒤에 사라짐
             $gopher.classList.add('hidden');
