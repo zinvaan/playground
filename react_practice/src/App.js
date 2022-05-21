d978111c9d681d9a31702da0e996b5ec4fad8da5
Your Name <dnjswls0506@naver.com>
2022-05-21 17:00:15 +0900

    리액트(리덕스 연습)

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
+import React from "react";
+import Counter from "./components/Counter";
+import Todos from "./components/Todos";
 
 const App = () => {
-  
-}
+  return (
+    <div>
+      <Counter number={0} />
+      <hr />
+      <Todos />
+    </div>
+  );
+};
 
 export default App;
