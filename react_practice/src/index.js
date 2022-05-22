f3f25a0ce250473fbaa545dcd9af896e751a5a57
Your Name <dnjswls0506@naver.com>
2022-05-23 01:05:07 +0900

    리액트(리덕스 적용하기)

--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- react_practice/src/modules/index.js
+++ react_practice/src/modules/index.js
@@ -1,4 +1,6 @@
 // rootReducer
+// createStore 함수를 사용하여 스토어를 만들 때는 리듀서를 하나만 사용해야 한다.
+// combindReducers라는 유틸 함수를 사용하여 리듀서를 하나로 합친다.
 import { combineReducers } from "redux";
 import counter from "./counter";
 import todos from "./todos";
