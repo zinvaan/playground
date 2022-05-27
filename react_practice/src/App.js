0684d5cb1fd9eaca1c93ab3b3b9adab7920fa11e
Your Name <dnjswls0506@naver.com>
2022-05-28 01:09:02 +0900

    리액트(hooks 복습)

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import Counter from "./components/Counter";
-import Todos from "./components/Todos";
+import React, { useState, useEffect, useMemo } from "react";
 
 const App = () => {
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
+  const location = useMemo(() => {
+    return {
+      country: isKorea ? "한국" : "외국",
+    };
+  }, [isKorea]);
+  useEffect(() => {
+    console.log("useEffect 호출");
+  }, [location]);
   return (
     <div>
-      <Counter number={0} />
+      <h2>하루에 몇끼 먹어요?</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
       <hr />
-      <Todos />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라: {location.country}</p>
+      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
     </div>
   );
 };
