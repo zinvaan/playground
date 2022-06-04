e1e26166c1218f0205cb993aae3471a0f947b50d
Your Name <dnjswls0506@naver.com>
2022-06-04 18:28:15 +0900

    리액트(라우터)

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import Home from './pages/Home';
-import About from './pages/About';
-import { BrownserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import Search from './pages/Search';
+import ListPorducts from './pages/ListProducts';
+import AddProduct from './pages/AddProduct';
+import ProductDisplay from './pages/ProductDisplay';
+import { Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <BrownserRouter>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/products/search">Products</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/search" element={<Search />} />
+        <Route path="/products/list" element={<ListPorducts />} />
+        <Route path="/products/add" element={<AddProduct />} />
+        <Route path="/products/:id" element={<ProductDisplay />} />
       </Routes>
-    </BrownserRouter>
+    </Router>
   );
 }
 
