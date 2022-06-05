1289d3ea61f2a5cd242111f2a7ac046b05b02b9f
Your Name <dnjswls0506@naver.com>
2022-06-05 16:41:53 +0900

    리액트(useNavigate 사용)

--- react_todolist/src/pages/About.js
+++ react_todolist/src/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const About = () => {
   const { search } = useLocation();
+  const navigate = useNavigate();
   // 현재 경로가(search) '?detail-true' 인지 확인
   const detail = search === '?detail=true';
+  const goBack = () => {
+    navigate(-1);
+  };
+  // 홈으로 가기
+  const goHome = () => {
+    navigate('/');
+  };
   return (
     <div>
       <h1>소개</h1>
@@ -12,6 +20,8 @@ const About = () => {
         이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트 입니다.
       </p>
       {detail && <p>detail 값을 true로 설정하셨군요!</p>}
+      <button onClick={goBack}>back</button>
+      <button onClick={goHome}>home</button>
     </div>
   );
 };
