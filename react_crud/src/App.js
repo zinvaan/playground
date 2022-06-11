5d615f54e681b2b84307bcf2f525d46449e8a692
Your Name <dnjswls0506@naver.com>
2022-06-11 23:53:20 +0900

    리액트(map 이해하기)

--- react_crud/src/App.js
+++ react_crud/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Header = ({ title }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>
+        <a href="/">{title}</a>
+      </h1>
+    </header>
+  );
+};
+const Nav = ({ topics }) => {
+  console.log("topics: ", topics);
+  const titleList = [];
+  // const lis = [
+  //   <li>
+  //     <a href="/read/1">html</a>
+  //   </li>,
+  //   <li>
+  //     <a href="/read/2">css</a>
+  //   </li>,
+  //   <li>
+  //     <a href="/read/3">javascript</a>
+  //   </li>,
+  // ];
+  let lis = titleList.map(() => {
+    return (
+      <li key={topics.id}>
+        <a href={"/read/" + topics.id}>{topics.title}</a>
+      </li>
+    );
+  });
+  console.log("titleList:", titleList);
+  console.log("lis:", lis);
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+};
+
+const Article = () => {
+  return (
+    <article>
+      <h2>welcom</h2>
+      hello, web
+    </article>
+  );
+};
+function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
+  return (
+    <div>
+      <Header title="REACT" body="Hello, Web" />
+      <Nav topics={topics} />
+      <Article />
     </div>
   );
 }
