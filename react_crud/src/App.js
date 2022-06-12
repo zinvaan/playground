85f186ff249da114d8a07b1234b843dc8bb8778f
Your Name <dnjswls0506@naver.com>
2022-06-12 23:29:45 +0900

    리액트(props로 전달한 list 작성)

--- react_crud/src/App.js
+++ react_crud/src/App.js
@@ -10,8 +10,8 @@ const Header = ({ title }) => {
   );
 };
 const Nav = (props) => {
+  const lis = [];
   for (let i = 0; i < props.topics.length; i++) {
-    const lis = [];
     let t = props.topics[i];
     lis.push(
       <li key={t.id}>
